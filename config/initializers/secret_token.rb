eddabbd8f33e8ce999ed62bfe8c47c1759dc24a7
Ben <benjamin.c.henderson@gmail.com>
2014-09-23 00:13:26 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EtymApp::Application.config.secret_key_base = '31c9de6982f0c9c8b07372742338b2774599dffa95e7557e957fbf6ab5d22e4b43b7a9680e1f5e1ad9995a8b3e75c5c570dcaa2144c133f4c1018e97f1d9d5e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EtymApp::Application.config.session_store :cookie_store, key: '_etym_app_session'
